71625ebcca0dc5165d25bc49e75c39ddb3afeca9
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2026-06-27 10:49:29 +0200

    ngdevkit sound effects

--- bneogeo.sh
+++ bneogeo.sh
@@ -112,14 +112,18 @@ rm w_wad.o
 rm z_zone.o
 
 m68k-neogeo-elf-objcopy -O binary -S -R .text2 --gap-fill 0xff --pad-to 1048576 neogeo/DOOM64KB.elf neogeo/rom/doom64kb-p1.p1 && dd if=neogeo/rom/doom64kb-p1.p1 of=neogeo/rom/doom64kb-p1.p1 conv=notrunc,swab status=none
-z80-neogeo-ihx-sdobjcopy -I ihex -O binary neogeo/assets/base-sound-driver.ihx neogeo/rom/doom64kb-m1.m1 --pad-to 131072
-echo neogeo/assets/base-crom-logo.c1 | xargs -r cat > neogeo/rom/doom64kb-c1.c1 && truncate -s 2097152 neogeo/rom/doom64kb-c1.c1
-echo neogeo/assets/base-crom-logo.c2 | xargs -r cat > neogeo/rom/doom64kb-c2.c2 && truncate -s 2097152 neogeo/rom/doom64kb-c2.c2
-echo neogeo/assets/ibmfont.fix | xargs -r cat > neogeo/rom/doom64kb-s1.s1 && truncate -s 131072 neogeo/rom/doom64kb-s1.s1
-echo | xargs -r cat > neogeo/rom/doom64kb-v1.v1 && truncate -s 524288 neogeo/rom/doom64kb-v1.v1
-romtool.py -b cartridge -f zip -p neogeo/rom/doom64kb-p1.p1 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -x "zip.comment=" -o neogeo/rom/doom64kb.zip
-romtool.py -b hash -f mame  -p neogeo/rom/doom64kb-p1.p1 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -l "Doom64KB: Neo Geo Edition" -o neogeo/rom/neogeo.xml
-romtool.py -b hash -f gngeo -p neogeo/rom/doom64kb-p1.p1 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -l "Doom64KB: Neo Geo Edition" -x gngeo.data=/usr/share/ngdevkit-gngeo/gngeo_data.zip -o neogeo/rom/gngeo_data.zip
+z80-neogeo-ihx-sdobjcopy -I ihex -O binary neogeo/assets/demo_driver.ihx neogeo/rom/doom64kb-m1.m1 --pad-to 131072
+cp neogeo/assets/base-crom-logo.c1 neogeo/rom/doom64kb-c1.c1
+cp neogeo/assets/base-crom-logo.c2 neogeo/rom/doom64kb-c2.c2
+cp neogeo/assets/ibmfont.fix neogeo/rom/doom64kb-s1.s1
+cp neogeo/assets/samples.v1 neogeo/rom/doom64kb-v1.v1
+truncate -s 2097152 neogeo/rom/doom64kb-c1.c1
+truncate -s 2097152 neogeo/rom/doom64kb-c2.c2
+truncate -s 131072 neogeo/rom/doom64kb-s1.s1
+truncate -s 524288 neogeo/rom/doom64kb-v1.v1
+romtool.py -b cartridge -f zip   -p neogeo/rom/doom64kb-p1.p1 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -x "zip.comment="              -o neogeo/rom/doom64kb.zip
+romtool.py -b hash      -f mame  -p neogeo/rom/doom64kb-p1.p1 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -l "Doom64KB: Neo Geo Edition" -o neogeo/rom/neogeo.xml
+romtool.py -b hash      -f gngeo -p neogeo/rom/doom64kb-p1.p1 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -l "Doom64KB: Neo Geo Edition" -o neogeo/rom/gngeo_data.zip -x gngeo.data=/usr/share/ngdevkit-gngeo/gngeo_data.zip
 cp /usr/share/ngdevkit/aes.zip    neogeo/rom/aes.zip
 cp /usr/share/ngdevkit/neogeo.zip neogeo/rom/neogeo.zip
 
