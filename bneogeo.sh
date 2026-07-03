7f6ab287bf5fc508651ca644ede4042d1b8f3a21
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2026-07-04 01:17:37 +0200

    fix build script to handle reciprocalTable correctly

--- bneogeo.sh
+++ bneogeo.sh
@@ -96,7 +96,7 @@ export GLOBOBJS+=" z_bmallo.c"
 #export GLOBOBJS+=" z_zone.c"
 export GLOBOBJS+=" z_zone.o"
 
-m68k-neogeo-elf-gcc -specs ngdevkit -lngdevkit $GLOBOBJS $CFLAGS $RENDER_OPTIONS -o neogeo/DOOM64KB.elf
+m68k-neogeo-elf-gcc -specs ngdevkit -lngdevkit $GLOBOBJS $CFLAGS $RENDER_OPTIONS -o neogeo/DOOM64KB.elf -Wl,--enable-non-contiguous-regions
 
 rm i_neogev.o
 rm p_enemy2.o
@@ -111,7 +111,8 @@ rm tables.o
 rm w_wad.o
 rm z_zone.o
 
-m68k-neogeo-elf-objcopy -O binary -S -R .text2 --gap-fill 0xff --pad-to 1048576 neogeo/DOOM64KB.elf neogeo/rom/doom64kb-p1.p1 && dd if=neogeo/rom/doom64kb-p1.p1 of=neogeo/rom/doom64kb-p1.p1 conv=notrunc,swab status=none
+m68k-neogeo-elf-objcopy -O binary -S -R .text2 --gap-fill 0xff --pad-to             1048576   neogeo/DOOM64KB.elf neogeo/rom/doom64kb-p1.p1 && dd if=neogeo/rom/doom64kb-p1.p1 of=neogeo/rom/doom64kb-p1.p1 conv=notrunc,swab status=none
+m68k-neogeo-elf-objcopy -O binary    -j .text2 --gap-fill 0xff --pad-to $((0x200000+1048576)) neogeo/DOOM64KB.elf neogeo/rom/doom64kb-p2.p2 && dd if=neogeo/rom/doom64kb-p2.p2 of=neogeo/rom/doom64kb-p2.p2 conv=notrunc,swab status=none
 z80-neogeo-ihx-sdobjcopy -I ihex -O binary neogeo/assets/demo_driver.ihx neogeo/rom/doom64kb-m1.m1 --pad-to 131072
 cp neogeo/assets/base-crom-logo.c1 neogeo/rom/doom64kb-c1.c1
 cp neogeo/assets/base-crom-logo.c2 neogeo/rom/doom64kb-c2.c2
@@ -121,9 +122,9 @@ truncate -s 2097152 neogeo/rom/doom64kb-c1.c1
 truncate -s 2097152 neogeo/rom/doom64kb-c2.c2
 truncate -s 131072 neogeo/rom/doom64kb-s1.s1
 truncate -s 524288 neogeo/rom/doom64kb-v1.v1
-romtool.py -b cartridge -f zip   -p neogeo/rom/doom64kb-p1.p1 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -x "zip.comment="              -o neogeo/rom/doom64kb.zip
-romtool.py -b hash      -f mame  -p neogeo/rom/doom64kb-p1.p1 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -l "Doom64KB: Neo Geo Edition" -o neogeo/rom/neogeo.xml
-romtool.py -b hash      -f gngeo -p neogeo/rom/doom64kb-p1.p1 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -l "Doom64KB: Neo Geo Edition" -o neogeo/rom/gngeo_data.zip -x gngeo.data=/usr/share/ngdevkit-gngeo/gngeo_data.zip
+romtool.py -b cartridge -f zip   -p neogeo/rom/doom64kb-p1.p1 neogeo/rom/doom64kb-p2.p2 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -x "zip.comment="              -o neogeo/rom/doom64kb.zip
+romtool.py -b hash      -f mame  -p neogeo/rom/doom64kb-p1.p1 neogeo/rom/doom64kb-p2.p2 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -l "Doom64KB: Neo Geo Edition" -o neogeo/rom/neogeo.xml
+romtool.py -b hash      -f gngeo -p neogeo/rom/doom64kb-p1.p1 neogeo/rom/doom64kb-p2.p2 -c neogeo/rom/doom64kb-c1.c1 neogeo/rom/doom64kb-c2.c2 -v neogeo/rom/doom64kb-v1.v1 -s neogeo/rom/doom64kb-s1.s1 -m neogeo/rom/doom64kb-m1.m1 -n doom64kb -l "Doom64KB: Neo Geo Edition" -o neogeo/rom/gngeo_data.zip -x gngeo.data=/usr/share/ngdevkit-gngeo/gngeo_data.zip
 cp /usr/share/ngdevkit/aes.zip    neogeo/rom/aes.zip
 cp /usr/share/ngdevkit/neogeo.zip neogeo/rom/neogeo.zip
 
